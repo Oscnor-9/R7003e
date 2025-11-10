80114d4a6222394112b487ffa917b815a3deb846
oscnor-9 <oscnor-9@student.ltu.segit cofig --global --listgit config --global user.email oscnor-9@student.ltu.se>
2025-11-10 11:23:35 +0100

    f

--- LabB_ObserverAndControllerOverSimulator_Continuous_Parameters.m
+++ LabB_ObserverAndControllerOverSimulator_Continuous_Parameters.m
@@ -6,30 +6,67 @@ kP = -404.3247
 kI = -2.2604e+03
 kD = -0.8411
 
-A = 1.0e+03 * [
-         0    0.0010         0         0
-         0   -0.4350   -0.0061    0.0091
-         0         0         0    0.0010
-         0    1.9034    0.0620   -0.0400
-]
+% Same parameter block as in Lab A
+g   = 9.8;
+m_b = 0.381;  l_b = 0.112;  I_b = 0.00616;
+m_w = 0.036;  l_w = 0.021;  I_w = 0.00000746;
+R_m = 4.4;    K_e = 0.444;  K_t = 0.470;
 
-B = [
-         0
-   20.6000
-         0
-  -90.0000
-]
+gamma_11 = l_w*(m_w + m_b) + (I_w/l_w);
+gamma_12 = l_w*l_b*m_b;
+gamma_21 = m_b*l_b;
+gamma_22 = I_b+m_b*l_b^2;
 
-C = [
-     1     0     0     0
-     0     0     1     0
-]
+alpha_11 = 0;
+alpha_12 = -((K_t*K_e)/(R_m*l_w));
+alpha_13 = 0;
+alpha_14 = ((K_t*K_e)/R_m);
+alpha_21 = 0;
+alpha_22 = (K_t*K_e)/(R_m*l_w);
+alpha_23 = m_b*l_b*g;
+alpha_24 = -((K_t*K_e)/R_m);
 
-D = [
-     0
-]
+beta_11 = K_t/R_m;
+beta_21 = -K_t/R_m;
+
+gamma = [gamma_11, gamma_12; gamma_21, gamma_22];
+alpha = [alpha_11, alpha_12, alpha_13, alpha_14;
+         alpha_21, alpha_22, alpha_23, alpha_24];
+beta  = [beta_11; beta_21];
+
+A_small = gamma\alpha;
+B_small = gamma\beta;
+
+A = [0 1 0 0;
+     A_small(1,:);
+     0 0 0 1;
+     A_small(2,:)];
+B = [0; B_small(1); 0; B_small(2)];
+
+C = [0 0 1 0];
+D = zeros(2,1);
+
+%task4.5.1
+O = obsv(A,C);
+rankO = rank(O);
+
+Ctl = ctrb(A, B);
+rankC = rank(Ctl);
+
+%task4.6.1
+pc = [-475.0690; -5.6571; -5.72; -70];
+K2 = acker(A,B,pc);
+
+%4.7.1
+rho = 1;
+CQ = [25,1,15,1];
+Q = rho*transpose(CQ)*CQ
+R = 1;
+
+K = lqr(A,B,Q,R)
 
 
+%task4.8...
 C_acc =[1 0 0 0];
 C_nacc = [0 0 1 0];
 T_inv=[C_acc;
@@ -55,69 +92,7 @@ C_x = C_tilde_nacc(2:end);
 CC = [Ayx; C_x];
 
 
-L = ( place( Axx', CC', afPoles ) )'
+%L = ( place( Axx', CC', afPoles ) )'
 
-
-
-
-
-
-
-
-
-% M1 = 1.0e+03 * [
-%    -0.4358   -0.0072    0.0091
-%    -0.0011   -0.0340    0.0010
-%     1.8717   -0.0165   -0.0400
-% ]
-% 
-% M2 = [
-%    20.6000
-%          0
-%   -90.0000
-% ]
-% 
-% M3 = [
-%      0
-%      0
-%      0
-% ]
-% 
-% M4 = [
-%     1.1459
-%    33.9942
-%    78.4584
-% ]
-% 
-% M5 = [
-%     0.7598
-%     1.1459
-%    31.6987
-% ]
-% 
-% M6 = [
-%      1
-%      0
-%      0
-%      0
-% ]
-% 
-% M7 = [
-%      0     0     0
-%      1     0     0
-%      0     1     0
-%      0     0     1
-% ]
-% 
-% L = [
-%    15.0196    0.5900
-%    -2.7287   18.9345
-%     0.6038   44.9804
-%    22.1715  435.4379
-% ]
-
-
-% 
-% K = [ -10.0000  -57.4908 -105.0371  -19.5009 ]
 
 
